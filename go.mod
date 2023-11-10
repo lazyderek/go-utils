23fd11365b0adff9107d5d91c59e6f39cf446a3b
derek <guinyin@foxmail.com>
2023-11-10 17:10:23 +0800

    mapKeyType

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/lazyderek/go-utils
 
-go 1.13
+go 1.18
 
 require (
 	github.com/go-redis/redis v6.15.9+incompatible
--- types/map.go
+++ types/map.go
@@ -1,24 +1,24 @@
 package types
 
-// mapKeyType map key的约束
-type mapKeyType interface {
+// MapKeyType map key的约束
+type MapKeyType interface {
 	~string |
 		~uint8 | ~int8 |
 		~uint16 | ~int16 |
 		~int | ~uint |
 		~uint32 | ~int32 |
 		~int64 | ~uint64 |
-		~float32
+		~float64
 }
 
 // GenericMap 泛型 map
-type GenericMap[K mapKeyType, V any] map[K]V
+type GenericMap[K MapKeyType, V any] map[K]V
 
-// MapKeySelector  [K mapKeyType, T any]
-// map key 选择器， mapKeyType 约束 key的类型，T 约束 value 的类型
-type MapKeySelector[K mapKeyType, V any] func(v V) K
+// MapKeySelector  [K MapKeyType, T any]
+// map key 选择器， MapKeyType 约束 key的类型，T 约束 value 的类型
+type MapKeySelector[K MapKeyType, V any] func(v V) K
 
-// Array2Map [K mapKeyType, V any]
+// Array2Map [K MapKeyType, V any]
 // 泛型函数，将一个切片转换为map, 可以自定义 key 值，
 // MapKeySelector 是自定义key的函数
 // 示例：
@@ -35,7 +35,7 @@ type MapKeySelector[K mapKeyType, V any] func(v V) K
 // fmt.Println(m)
 // Output:
 // map[1:{i} 2:{2}]
-func Array2Map[K mapKeyType, V any](
+func Array2Map[K MapKeyType, V any](
 	kSelector MapKeySelector[K, V], // key选择器，自定义函数
 	array []V, // 数组
 ) GenericMap[K, V] {
